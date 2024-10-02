f48c96e05c6570b52efe8c414161217c6eacd215
CodingMileage <72987158+CodingMileage@users.noreply.github.com>
2024-10-02 13:32:24 -0500

    Orders Page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,4 +1,7 @@
 import { prisma } from "@/lib/db/prisma";
+import { Container } from "@mui/material";
+import Link from "next/link";
+import { formatPrice } from "@/lib/format";
 
 interface OrdersProps {
   userId: string; // Expecting userId as a prop
@@ -11,33 +14,48 @@ export default async function Orders({ userId }: OrdersProps) {
   });
 
   return (
-    <div>
+    <Container maxWidth="lg">
       <h1>Orders for User ID: {userId}</h1>
       {orders.length === 0 ? (
         <p>No orders found for this user.</p>
       ) : (
         orders.map((order) => (
           <div key={order.id} className="border p-4 mb-4">
-            <h2 className="font-bold">Order ID: {order.id}</h2>
-            <ul className="list-disc ml-6">
+            <div className="flex justify-between p-2 rounded bg-slate-400">
+              {/* Display the order information */}
+              <h2 className="">
+                <span className="font-bold">Order Total:</span>{" "}
+                <span className="">{formatPrice(order.totalAmount)}</span>
+              </h2>
+              <h2>
+                <span className="font-bold">Order ID:</span> {order.id}
+              </h2>
+            </div>
+            <ul className="ml-6">
               {order.items.map((item) => (
-                <li key={item.id} className="mb-2">
-                  <strong>Product Name:</strong> {item.product.name} <br />
-                  <strong>Quantity:</strong> {item.quantity} <br />
-                  <strong>Price:</strong> ${item.price / 100} <br />
+                <li key={item.id} className="flex mt-2 mb-2">
+                  {/* Dynamically link to each product */}
                   {item.product.imageUrl && (
-                    <img
-                      src={item.product.imageUrl}
-                      alt={item.product.name}
-                      style={{ width: "100px", borderRadius: "8px" }}
-                    />
+                    <Link href={`/products/${item.product.id}`}>
+                      <img
+                        src={item.product.imageUrl}
+                        alt={item.product.name}
+                        style={{ width: "100px", borderRadius: "8px" }}
+                      />
+                    </Link>
                   )}
+                  {/* Product name */}
+                  <div className="ml-4">
+                    <Link href={`/products/${item.product.id}`}>
+                      <h3>{item.product.name}</h3>
+                    </Link>
+                  </div>
                 </li>
               ))}
             </ul>
           </div>
         ))
       )}
-    </div>
+    </Container>
   );
 }
