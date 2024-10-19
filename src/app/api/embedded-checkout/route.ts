b8fa4f1249405617fc28619eba6a71cf8ce8558a
CodingMileage <72987158+CodingMileage@users.noreply.github.com>
2024-10-19 09:52:51 -0500

    Emb

---
+++ src/app/api/embedded-checkout/route.ts
@@ -0,0 +1,85 @@
+import { prisma } from "@/lib/db/prisma";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Stripe from "stripe";
+import { getCart } from "@/lib/db/cart";
+import { Resend } from "resend";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {});
+const resend = new Resend(process.env.RESEND_API_KEY as string);
+
+export async function POST(request: Request) {
+  let event;
+
+  try {
+    // Verify the webhook event using Stripe's secret
+    event = await stripe.webhooks.constructEvent(
+      await request.text(),
+      request.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+  } catch (err) {
+    console.error("Error verifying webhook signature:", err);
+    return new NextResponse("Webhook Error", { status: 400 });
+  }
+
+  try {
+    const { cartId } = await request.json();
+
+    // Retrieve the cart using Prisma, including variant and product info
+    const cart = await getCart();
+
+    // If the cart is not found or is empty, return a 404
+    if (!cart || !cart.items || cart.items.length === 0) {
+      return NextResponse.json(
+        { message: "Cart not found or is empty" },
+        { status: 404 }
+      );
+    }
+
+    // Retrieve the user session
+    const session = await getServerSession(authOptions);
+
+    // Prepare Stripe line items from the cart items
+    const lineItems = cart.items.map((item) => {
+      const selectedVariant = item.product.variants.find(
+        (variant) => variant.id === item.variantId
+      );
+
+      return {
+        price_data: {
+          currency: "USD",
+          product_data: {
+            name: item.product.name + " x " + item.quantity, // Get the product name
+            // description: selectedVariant?.product.description ?? "", // Optional description
+          },
+          unit_amount: selectedVariant?.price, // Use the specific variant price in cents
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    // Create a Stripe Checkout session
+    const stripeSession = await stripe.checkout.sessions.create({
+      ui_mode: "embedded",
+      payment_method_types: ["card"],
+      automatic_tax: { enabled: true },
+      line_items: lineItems,
+      mode: "payment",
+      metadata: {
+        userId: session?.user?.id ?? "unknown", // Attach user ID or default
+        email: session?.user?.email ?? "unknown", // Attach user email or default
+      },
+      return_url: `${request.headers.get("origin")}/stripe/purchase-success`,
+    });
+
+    return NextResponse.json({
+      id: stripeSession.id,
+      client_secret: stripeSession.client_secret,
+    });
+  } catch (error: any) {
+    console.error("Stripe session error:", error.message);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import {
   TrendingSparkle,
 } from "@/components/Nyxb/Sparkle";
 import Hero from "@/components/Hero";
+import EmbeddedCheckoutButton from "@/components/EmbeddedCheckout";
 
 // Define the ProductVariant type
 type ProductVariant = {
@@ -105,6 +106,7 @@ export default async function Home() {
     <>
       {/* Hero Section */}
       <Hero />
+      <EmbeddedCheckoutButton />
 
       {/* Main Product Grid */}
       <div className="bg-emerald-300 rounded">
