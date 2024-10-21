bcfe6175b3d40a6caa9bcebeb893bc6e0e93badf
CodingMileage <72987158+CodingMileage@users.noreply.github.com>
2024-10-21 13:20:51 -0500

    Products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ import ClientProductPage from "./ClientProductPage";
 export default async function ProductPage() {
   // Fetch products from Prisma
   const products = await prisma.product.findMany({
-    take: 8,
+    take: 9,
     where: { comingSoon: false },
     include: {
       variants: true, // Include ProductVariant relation
